3f17d3a10f66f32ffe9d9e1ac24b7749cd94542d
RubenFilipe2002 <32169200+RubenFilipe2002@users.noreply.github.com>
2017-10-26 17:01:21 +0100

    Add files via upload

---
+++ operaçoesaritmeticas.java
@@ -0,0 +1,13 @@
+package pacote1;
+
+public class operaçoesaritmeticas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.printf("%d\n",4+3*(15-2));
+System.out.printf("%f\n",40.0/9);
+System.out.printf("%d\n",40%3);
+
+	}
+
+}
